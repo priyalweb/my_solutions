6d980d78e188135a88dba6642fc4dc89f8417861
PriyalDubey <priyaldubey2000@gmail.com>
2020-10-11 00:43:06 +0530

    Q1 Pairs Having Similar Elements

---
+++ q1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int similarPair(int arr[], int n){
+	sort(arr,arr+n);
+	long long int same =0, count =0,total =0;
+	
+	for(int i=0;i<n;i++){
+		if( arr[i+1]-arr[i]== 1 ){
+			count++;
+			
+		}
+		else if(arr[i]==arr[i+1]){
+			same++;
+//			continuous = 0;
+		}
+		else{
+			if(same>=1 & count>0){
+				count = count + same;
+			}
+			total = total + (count+1)*(count+1-1)/2 ; 
+//			continuous = 0;
+			same =0; count =0;
+		}
+	}
+	return total;
+}
+
+
+int main(){
+
+int n;
+cin>>n;
+int arr[n];
+for(int p=0;p<n;p++){
+	cin>>arr[p];
+}
+
+cout<<similarPair(arr,n);
+    return 0;
+}
